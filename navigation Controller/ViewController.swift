618ac52a9124b9311b358a8e7c6d662797d3e64f
shashwat-code <71892990+shashwat-code@users.noreply.github.com>
2021-09-29 15:26:41 +0530

    navi

--- navigation Controller/ViewController.swift
+++ navigation Controller/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var btn = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        btn.setTitle("click here for present", for: .normal)
+        btn.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        btn.setTitleColor(.black, for: .normal)
+        btn.backgroundColor = .white
+        view.addSubview(btn)
+        view.backgroundColor = .systemGray6
+        title = "Home view controllers"
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        btn.frame = CGRect(x: view.frame.midX - (view.frame.size.width)/2 + 10, y: view.frame.midY, width: view.frame.size.width-20, height: 50)
+    }
+    
+    @objc func didTapButton(){
+        let vc  = secondViewControllers()
+        let nav = UINavigationController(rootViewController: vc)
+        //nav.pushViewController(vc, animated: true)
+        present(nav, animated: true)
     }
 
+}
 
+class secondViewControllers: UIViewController {
+    
+    var btn = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Second Veiw Controller"
+        view.backgroundColor = .systemBlue
+        btn.setTitle("push another function", for: .normal)
+        btn.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        btn.setTitleColor(.black, for: .normal)
+        btn.backgroundColor = .white
+        view.addSubview(btn)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "dismiss", style: .done, target: self, action: #selector(dismiss1))
+    }
+    
+    @objc func dismiss1(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        btn.frame = CGRect(x: view.frame.midX - (view.frame.size.width - 20)/2, y: view.frame.midY, width: view.frame.size.width-20, height: 50)
+    }
+    @objc func didTapButton(){
+        let vc  = ViewController()
+      //  let nav = UINavigationController(rootViewController: vc)
+        navigationController?.pushViewController(vc, animated: true)
+        //let nav = UINavigationController(rootViewController: vc)
+        //nav.pushViewController(vc, animated: true)
+        //present(nav, animated: true)
+    }
 }
 
